b0bd895f28d3e03b7b206592a66b7eaa141c0399
jesus eusse <jesuseusse@gmail.com>
2025-06-28 12:57:00 -0600

    Theme dark and light

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'color_palette.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.lightPrimary,
+      brightness: Brightness.light,
+    ),
+    scaffoldBackgroundColor: AppColors.lightBackground,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.white,
+      foregroundColor: Colors.black,
+    ),
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.darkPrimary,
+      brightness: Brightness.dark,
+    ),
+    scaffoldBackgroundColor: AppColors.darkBackground,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color(0xFF1F1F1F),
+      foregroundColor: Colors.white,
+    ),
+  );
+}
---
+++ lib/core/theme/color_palette.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Light mode
+  static const lightBackground = Color(0xFF000000);
+  static const lightPrimary = Color(0xFF1DCD9F);
+  static const lightSecondary = Color(0xFF169976);
+
+  // Dark mode
+  static const darkBackground = Color(0xFF000000);
+  static const darkPrimary = Color(0xFF1DCD9F);
+  static const darkSecondary = Color(0xFF169976);
+}
